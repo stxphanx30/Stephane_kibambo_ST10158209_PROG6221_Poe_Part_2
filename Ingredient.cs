8cf3cfbc81d48e02760649b9e44f3582f380ab56
stxphanx30 <ST10158209@vcconnect.edu.za>
2024-05-30 15:42:36 +0200

    update of ingredients class with new attributes

--- Ingredient.cs
+++ Ingredient.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Poe_part_2
 {
+
     public class Ingredient
     {
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        public string Unit { get; set; }
-        public double Calories { get; set; }
-        public string FoodGroup { get; set; }
+        public string Name { get; }
+    public double Quantity { get; set; }
+    public string Unit { get; }
+    public double Calories { get; }
+    public string FoodGroup { get; }
+    private double originalQuantity;
+
+    public Ingredient(string name, double quantity, string unit, double calories, string foodGroup)
+    {
+        Name = name;
+        Quantity = quantity;
+        Unit = unit;
+        Calories = calories;
+        FoodGroup = foodGroup;
+        originalQuantity = quantity;
     }
+
+    public void ResetQuantity()
+    {
+        Quantity = originalQuantity;
+    }
+}
 }
